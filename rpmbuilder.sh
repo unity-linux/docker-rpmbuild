02059b8983faa29280e062e4dce76127c38f4c06
Mark McKinstry <mmckinst@umich.edu>
2016-03-23 22:23:53 -0400

    set the %_topdir macro more intelligently

--- rpmbuilder.sh
+++ rpmbuilder.sh
@@ -25,8 +25,11 @@ if [[ $rpmbuild_dir_gid != $builder_gid ]]; then
    groupmod -g $rpmbuild_dir_gid builder
 fi
 
-su builder -l -c "echo '%_topdir /rpmbuild/' >> ~/.rpmmacros"
+if ! grep -q '%_topdir /rpmbuild/' ~/.rpmmacros; then
+    su builder -l -c "echo '%_topdir /rpmbuild/ # docker-rpmbuilder' >> ~/.rpmmacros"
+fi
 su builder -l -c "rpmdev-setuptree"
 su builder -l -c "spectool -R -g  /rpmbuild/SPECS/*spec"
 su builder -l -c "find /rpmbuild/SPECS/*spec -print0 | xargs -0 mock -r ${MOCK_CONFIG} --resultdir=/tmp/ --buildsrpm --sources /rpmbuild/SOURCES/ --spec"
 su builder -l -c "mock -r ${MOCK_CONFIG} --resultdir=/rpmbuild/results/${MOCK_CONFIG} ${MOCK_CLI_OPTIONS} --rebuild /tmp/*src.rpm"
+su builder -l -c "sed -i '/%_topdir \/rpmbuild\/ # docker-rpmbuilder/d' ~/.rpmmacros"
