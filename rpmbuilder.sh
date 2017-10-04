98a63098ee0c277a916f743a8d536ebfc3988d13
unitylinux <jeremiah.summers@unity-linux.org>
2017-10-04 15:15:21 -0700

    - Add more quotes

--- rpmbuilder.sh
+++ rpmbuilder.sh
@@ -40,6 +40,6 @@ if ! grep -q '%_topdir /rpmbuild/' ~/.rpmmacros; then
 fi
 su builder -l -c "rpmdev-setuptree"
 su builder -l -c "spectool -R -g  /rpmbuild/*.spec"
-su builder -l -c "find /rpmbuild/*.spec -print0 | xargs -0 mock -D \""--define _sourcedir /rpmbuild\"" -r ${MOCK_CONFIG} --resultdir=/rpmbuild --buildsrpm --sources /rpmbuild --spec"
+su builder -l -c "find /rpmbuild/*.spec -print0 | xargs -0 mock -D \""--define\"" \""_sourcedir /rpmbuild\"" -r ${MOCK_CONFIG} --resultdir=/rpmbuild --buildsrpm --sources /rpmbuild --spec"
 su builder -l -c "mock -r ${MOCK_CONFIG} --resultdir=/rpmbuild/${MOCK_CONFIG} ${MOCK_CLI_OPTIONS} --rebuild /rpmbuild/*.src.rpm"
 su builder -l -c "sed -i '/%_topdir \/rpmbuild\/ # docker-rpmbuilder/d' ~/.rpmmacros"
