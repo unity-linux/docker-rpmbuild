099f6908cb7162fdb1e34325918dc5d710fa51a3
unitylinux <jeremiah.summers@unity-linux.org>
2017-10-04 16:46:26 -0700

    - Get rid of some non needed line

--- rpmbuilder.sh
+++ rpmbuilder.sh
@@ -39,7 +39,6 @@ if ! grep -q '%_topdir /rpmbuild/' ~/.rpmmacros; then
     su builder -l -c "echo '%_topdir /rpmbuild/' >> ~/.rpmmacros"
 fi
 su builder -l -c "rpmdev-setuptree"
-su builder -l -c "spectool -R -g  /rpmbuild/*.spec"
-su builder -l -c "find /rpmbuild/*.spec -print0 | xargs -0 mock -D \""--define\ _sourcedir\ /rpmbuild\"" -r ${MOCK_CONFIG} --resultdir=/rpmbuild --buildsrpm --sources /rpmbuild --spec"
-su builder -l -c "mock -r ${MOCK_CONFIG} --resultdir=/rpmbuild/${MOCK_CONFIG} ${MOCK_CLI_OPTIONS} --rebuild /rpmbuild/*.src.rpm"
+su builder -l -c "rpmbuild \""--undefine=_disable_source_fetch\"" \""--define\ _sourcedir\ /rpmbuild \"" \""--define\ _topdir\ /rpmbuild \"" -bs *.spec"
+su builder -l -c "mock -r ${MOCK_CONFIG} --resultdir=/rpmbuild/${MOCK_CONFIG} ${MOCK_CLI_OPTIONS} --rebuild /rpmbuild/SRPMS/*.src.rpm"
 su builder -l -c "sed -i '/%_topdir \/rpmbuild\/ # docker-rpmbuilder/d' ~/.rpmmacros"
